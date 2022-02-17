0480f0468c6a024f30eb635c07e2bbed53c70d54
nevoopdev <nevoopdev@gmail.com>
2022-02-17 20:12:01 +0530

    make it better

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ db.on('error',error => console.log(error))
 db.once('open',error => console.log("connected to mongoose"))
 
 app.set("view engine", "ejs")
-app.set("views", __dirname+'    /views')
+app.set("views", __dirname+'/views')
 app.set("layout","layouts/layout")
 app.use(expressLayout)
 app.use(express.static("public"))
